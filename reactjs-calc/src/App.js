915e9c8ade9ac488e7e0fb92c4b908f2150ac167
VasilG333 <107360902+VasilG333@users.noreply.github.com>
2022-08-18 13:20:25 +0300

    Somewhat fixed styling
    still ugly but itll do the job

--- reactjs-calc/src/App.js
+++ reactjs-calc/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { Button, ButtonGroup } from '@mui/material'
+import { Button, ButtonGroup, Box } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 const StyledButton = styled(Button)({
   color: 'darkslategray',
-  backgroundColor: 'aliceblue',
+  background: 'rgba(255, 255, 255, 0.7)',
   margin: '2px',
   borderRadius: '0',
   padding: '15px'
 })
 
+const StyledInput = styled(Box)({
+  display: 'block',
+  width: '500px',
+  height: '75px',
+  background: 'rgba(255, 255, 255, 0.8)'
+})
+
 const doubleSpanStyle = {
-  // display: 'inline-grid',
   'grid-column': '1 / span 2',
 }
 
@@ -19,36 +25,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGroup sx={{
-          display: 'grid',
-          margin: '0',
-          padding: '0px',
-          gridTemplateRows: 'repeat(5, 1fr)',
-          gridTemplateColumns: 'repeat(4, 1fr)',
-          alignItems: 'baseline'
-        }}>
-          <StyledButton sx={doubleSpanStyle}>AC</StyledButton>
-          <StyledButton>DEL</StyledButton>
-          <StyledButton>*</StyledButton>
-          <StyledButton>1</StyledButton>
-          <StyledButton>2</StyledButton>
-          <StyledButton>3</StyledButton>
-          <StyledButton>/</StyledButton>
-          <StyledButton>4</StyledButton>
-          <StyledButton>5</StyledButton>
-          <StyledButton>6</StyledButton>
-          <StyledButton>+</StyledButton>
-          <StyledButton>7</StyledButton>
-          <StyledButton>8</StyledButton>
-          <StyledButton>9</StyledButton>
-          <StyledButton>-</StyledButton>
-          <StyledButton>0</StyledButton>
-          <StyledButton>.</StyledButton>
-          <StyledButton sx={{
-            gridArea: '5 / 3 / 5 / 5'
-          }}>=</StyledButton>
-
-        </ButtonGroup>
+        <div className='calc'>
+          <StyledInput sx={{
+            display: 'block',
+            width: 'auto',
+          }} />
+          <ButtonGroup sx={{
+            display: 'grid',
+            gridTemplateRows: 'repeat(5, 1fr)',
+            gridTemplateColumns: 'repeat(4, 1fr)',
+            alignItems: 'baseline'
+          }}>
+            <StyledButton sx={doubleSpanStyle}>AC</StyledButton>
+            <StyledButton>DEL</StyledButton>
+            <StyledButton>*</StyledButton>
+            <StyledButton>1</StyledButton>
+            <StyledButton>2</StyledButton>
+            <StyledButton>3</StyledButton>
+            <StyledButton>/</StyledButton>
+            <StyledButton>4</StyledButton>
+            <StyledButton>5</StyledButton>
+            <StyledButton>6</StyledButton>
+            <StyledButton>+</StyledButton>
+            <StyledButton>7</StyledButton>
+            <StyledButton>8</StyledButton>
+            <StyledButton>9</StyledButton>
+            <StyledButton>-</StyledButton>
+            <StyledButton>0</StyledButton>
+            <StyledButton>.</StyledButton>
+            <StyledButton sx={{
+              gridArea: '5 / 3 / 5 / 5'
+            }}>=</StyledButton>
+          </ButtonGroup>
+        </div>
       </header>
     </div>
   );
